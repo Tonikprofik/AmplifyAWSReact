08090fabfb716d9cf48d21c052441d66a8b99437
PonikTrofik <izanami369@gmail.com>
2021-01-08 20:39:56 +0100

    cognito addtogroup.js

--- amplify/backend/function/amplifyawsreact81006319PostConfirmation/src/add-to-group.js
+++ amplify/backend/function/amplifyawsreact81006319PostConfirmation/src/add-to-group.js
@@ -1,31 +1,41 @@
-/* eslint-disable-line */ 
+/* eslint-disable-line */
 
-const aws = require('aws-sdk');
-
+const aws = require("aws-sdk");
 
 exports.handler = async (event, context, callback) => {
-  const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ apiVersion: '2016-04-18' });
-  const groupParams = {
-    GroupName: process.env.GROUP,
-    UserPoolId: event.userPoolId,
-  };
+  const cognitoProvider = new aws.CognitoIdentityServiceProvider({
+    apiVersion: "2016-04-18",
+  });
 
-  const addUserParams = {
-    GroupName: process.env.GROUP,
-    UserPoolId: event.userPoolId,
-    Username: event.userName,
-  };
+  let isAdmin = false;
+  const adminEmails = ["izanami369@gmail.com"];
 
-  try {
-    await cognitoidentityserviceprovider.getGroup(groupParams).promise();
-  } catch (e) {
-    await cognitoidentityserviceprovider.createGroup(groupParams).promise();
+  if (adminEmails.indexOf(event.request.userAttributes.email) !== -1) {
+    isAdmin = true;
+  }
+
+  if (isAdmin) {
+    const groupParams = {
+      GroupName: "Admin",
+      UserPoolId: event.userPoolId,
+    };
+    const userParams = {
+      GroupName: "Admin",
+      UserPoolId: event.userPoolId,
+      Username: event.username,
+    };
   }
 
   try {
-    await cognitoidentityserviceprovider.adminAddUserToGroup(addUserParams).promise();
+    await cognitoProvider.getGroup(groupParams).promise();
+  } catch (e) {
+    await cognitoProvider.createGroup(groupParams).promise();
+  }
+
+  try {
+    await cognitoProvider.adminAddUserToGroup(userParams).promise();
     callback(null, event);
   } catch (e) {
     callback(e);
-  }
+  } 
 };
